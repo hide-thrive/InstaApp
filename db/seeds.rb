d3659f751edf290aa01c810de555d3b8aade2a21
hide <ho.0628@icloud.com>
2022-01-03 20:16:06 +0900

    add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,11 +6,6 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-5.times do |n|
-  User.create!(
-    name: "user#{n + 1}",
-    email: "user#{n + 1}@example.com",
-    password: 'password'
-    password_confirmation: 'password'
-  )
-end
+require './db/seeds/users'
+require './db/seeds/posts'
+
